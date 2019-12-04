b46e276931fd687e0a7c994fe90fcb17cbce4ec8
Zach Burky <burkyza@cs.utexas.edu>
2019-12-04 07:07:47 +0000

    incomplete refactoring for use with Territories

--- app/config.py
+++ app/config.py
@@ -4,3 +4,7 @@ HERE_app_id = 'kO1cowFWMjjOgIKv6cFd'
 HERE_app_code = '_llArvZJD-XXKWs4q41hkQ'
 
 geolocator = Here(HERE_app_id, HERE_app_code)
+
+states_fips = '../data/state_fips.csv'
+county_fips = '../data/county_fips.csv'
+county_locs = '../data/county_loc.csv'
--- app/db.py
+++ app/db.py
@@ -1,51 +1,53 @@
 import pandas as pd
 from geopy.geocoders.base import GeocoderTimedOut
 
-from config import geolocator
+from config import geolocator, states_fips, county_fips, county_locs
 
 
 class County:
+    states = generate_states_dict(states_fips)
+
     def __init__(self, name, county_code, state_code):
         self.name = name
         self.county_code = county_code
         self.state_code = state_code
+        self.latitude = 0
+        self.longitude = 0
 
-
-    def lookup(self):
-        query = {'county': name,
-                'state': states[county['State Code']]}
-        lat, lon = 0, 0
+    def lookup(self, states):
+        query = {'county': self.name,
+                 'state': states[self.state_code]}
         try:
             obj = geolocator.geocode(query)
-            lat, lon = obj.latitude, obj.longitude
+            self.latitude = obj.latitude
+            self.longitude = obj.longitude
         except AttributeError:
             print(
-                f"-------------------------\n{county['Name']}, {states[county['State Code']]} not found.")
+                f"-------------------------\n{self.name}, {states[self.state_code]} not found.")
         except GeocoderTimedOut:
             print(
-                f"-------------------------\nHERE timed out. on {county['Name']}, {states[county['State Code']]}")
-        finally:
-            return lat, lon
+                f"-------------------------\nHERE timed out. on {self.name}, {states[self.state_code]}")
 
 
-    def generate_states_dict(filename):
-        states = pd.read_csv(filename,
-                            dtype=object).set_index('State Code')['Name'].to_dict()
+def generate_states_dict(filename):
+    return pd.read_csv(filename,
+                       dtype=object).set_index('State Code')['Name'].to_dict()
 
 
-    def generate_counties_df(filename):
-        counties = pd.read_csv('data/county_fips.csv', dtype=object)
-        counties.loc[counties['State Code'] == '72',
-                    'Name'] = ''  # allows lookup of Puerto Rico
-        return counties
+def generate_counties_list(filename):
+    counties = pd.read_csv(filename, dtype=object)
+    counties.loc[counties['State Code'] == '72',
+                 'Name'] = ''  # allows lookup of Puerto Rico
+    return counties
 
 
-    def write_counties(counties, destination):
-        counties.to_csv(destination, index=False)
+def write_counties(counties, destination):
+    counties.to_csv(destination, index=False)
 
 
-    def create_county_loc():
-        generate_states_dict('data/state_fips.csv')
-        counties = generate_counties_df('data/')
-        counties['Latitude'], counties['Longitude'] = zip(
-            *counties.apply(lookup, axis=1))
+def create_county_loc():
+    generate_states_dict(states_fips)
+    counties = generate_counties_df(county_fips)
+    counties['Latitude'], counties['Longitude'] = zip(
+        *counties.apply(lookup, axis=1))
+    write_counties(counties, county_locs)
