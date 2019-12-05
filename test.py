427e7516784175596adfe7d929cadee127389d60
Zach Burky <burkyza@cs.utexas.edu>
2019-12-05 13:34:57 +0000

    completed production of 2019 maps

--- test.py
+++ test.py
@@ -86,7 +86,7 @@ teams_venues = pd.merge(teams_2019, venues, on='id', how='outer')
 teams_venues['location'] = teams_venues['location'].map(eval)
 teams_venues = pd.concat([teams_venues.drop(columns=['location']), teams_venues['location'].apply(pd.Series)], axis=1)
 teams_venues = teams_venues.rename(columns={'x': 'Latitude', 'y': 'Longitude'})
-teams_venues.to_csv('data/teams_venues.csv')
+teams_venues.to_csv('data/teams_venues.csv', index=False)
 
 
 # %%
@@ -102,6 +102,56 @@ for _, row in teams_venues.iterrows():
 
 counties['Owner'] = counties[counties.columns.difference(['Name', 'Latitude', 'Longitude', 'FIPS'])].idxmin(axis=1)
 counties = counties[['Name', 'FIPS', 'Owner']]
-counties.to_csv('data/starting_counties.csv')
+counties.to_csv('data/starting_counties.csv', index=False)
+
+# %%
+teams_venues
+
+# %%
+import pandas as pd
+import plotly.figure_factory as ff
+
+teams_venues = pd.read_csv('data/teams_venues.csv')
+
+def empire_map(counties, name):
+    teams_venues = pd.read_csv('data/teams_venues.csv')
+    plotter = pd.merge(counties, teams_venues, left_on='Owner', right_on='school', how='outer')[['Name', 'FIPS', 'Owner', 'color', 'alt_color', 'logos']]
+    teams_venues = teams_venues.sort_values(by='school')
+    plotter = plotter.sort_values(by='Owner')
+    plotter = plotter.dropna()
+    fig = ff.create_choropleth(
+        fips=plotter['FIPS'].tolist(), 
+        values=plotter['Owner'].tolist(), 
+        colorscale=teams_venues[teams_venues['school'].isin(set(plotter['Owner']))]['color'].tolist(),
+        show_state_data=False, county_outline={'color': 'rgb(255,255,255)', 'width': 0.05}, title=f'Week {name}')
+    fig.write_image(f'images/week{name}.png')
+
+# %%
+games = pd.read_csv('data/games.csv', encoding='latin-1')
+games = games[['week', 'home_team', 'home_points', 'away_team', 'away_points']]
+
+# %%
+winner = lambda game: game['home_team'] if game['home_points'] > game['away_points'] else game['away_team']
+loser = lambda game: game['home_team'] if game['home_points'] < game['away_points'] else game['away_team']
+games['winner'] = games.apply(winner, axis=1)
+games['loser'] = games.apply(loser, axis=1)
+games = games[games['winner'].isin(teams_venues['school'])]
+
+# %%
+counties = pd.read_csv('data/starting_counties.csv')
+
+for week in range(1, 15):
+    for _, game in games[games['week'] == week].iterrows():
+        counties['Owner'] = counties['Owner'].replace({game['loser']: game['winner']})
+    empire_map(counties, week)
+    counties.to_csv(f'data/counties_week{week}.csv', index=False)
+
+# %%
+week_14 = pd.read_csv('data/counties_week14.csv', dtype=object)
+empire_map(week_14)
+
+# %%
+counties = pd.read_csv('data/starting_counties.csv')
+counties
 
 # %%
